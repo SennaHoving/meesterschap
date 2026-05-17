82b8fed9deb3766606f73821d260482efd8d5d8d
Senna Hoving <senna.hoving@gmail.com>
2026-05-17 23:06:48 +0200

    changes

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
-console.log("Hello, World!");
+/* bubbles */ 
+for (let i = 0; i < 24; i++) {
+    const bubble = document.createElement('div');
+    bubble.className = 'bubble';
+    const size = Math.random() * 18 + 6;
+    bubble.style.cssText = `
+        width: ${size}px;
+        height: ${size}px;
+        left: ${Math.random() * 100}%;
+        animation-duration: ${Math.random() * 12 + 10}s;
+        animation-delay: ${Math.random() * 16}s;
+    `;
+    document.body.appendChild(bubble);
+}
